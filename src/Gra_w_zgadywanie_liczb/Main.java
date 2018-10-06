8ed670adf03fdebaab93fc9251adf20f6bd5d10a
danielzurek <danielzurek1@gmail.com>
2018-10-06 12:22:59 +0200

    Warsztaty_1

--- src/Gra_w_zgadywanie_liczb/Main.java
+++ src/Gra_w_zgadywanie_liczb/Main.java
@@ -3,6 +3,8 @@ package Gra_w_zgadywanie_liczb;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Zgadnij liczbę");
+
     }
 }
